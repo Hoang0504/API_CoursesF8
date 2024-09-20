a8a238163f957a51cb56e8c4175e4eae9271e5bb
Hoang0504 <ngochoanghuy0504@gmail.com>
2024-09-20 20:06:11 +0700

    2009-3

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
 mongoose
-    .connect('mongodb+srv://hoangtnh2209:KVE4llSTiAD6EQLo@db.l2kjc.mongodb.net/?retryWrites=true&w=majority&appName=db')
+    .connect(
+        'mongodb+srv://hoangtnh2209:KVE4llSTiAD6EQLo@db.l2kjc.mongodb.net/f8Db?retryWrites=true&w=majority&appName=db',
+    )
     .then(() => {
         console.log('Connected to MongoDB');
     })
